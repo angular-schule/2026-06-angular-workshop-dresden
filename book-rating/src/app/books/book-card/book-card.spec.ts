4c004c6798bcd9c3bde0cd69fa871f5f718d2da9
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-02 14:51:04 +0200

    Alle Tests grün!

--- book-rating/src/app/books/book-card/book-card.spec.ts
+++ book-rating/src/app/books/book-card/book-card.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookCard } from './book-card';
+import { inputBinding } from '@angular/core';
 
 describe('BookCard', () => {
   let component: BookCard;
@@ -11,8 +12,26 @@ describe('BookCard', () => {
       imports: [BookCard],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(BookCard);
+    fixture = TestBed.createComponent(BookCard, {
+      bindings: [
+        inputBinding('book', () => ({
+          isbn: '',
+          description: '',
+          title: '',
+          rating: 3,
+          price: 33,
+          authors: []
+        }))
+      ]
+    });
+    // TS-Klasseninstanz
     component = fixture.componentInstance;
+
+    // DOM-Element
+    // fixture.nativeElement
+
+    
+
     await fixture.whenStable();
   });
 
