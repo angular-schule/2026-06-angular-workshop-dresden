5330bf7ded21f3c31451bac75b621b68e6c17132
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-03 14:16:18 +0200

    Routing

---
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-book-details-page',
+  imports: [RouterLink],
+  templateUrl: './book-details-page.html',
+  styleUrl: './book-details-page.scss',
+})
+export class BookDetailsPage {}
