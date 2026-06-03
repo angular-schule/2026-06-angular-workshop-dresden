cfb046fef9c672fa01a76e72477b25ec10978467
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-03 10:33:17 +0200

    Buttons deaktivieren

--- book-rating/src/app/books/book-card/book-card.ts
+++ book-rating/src/app/books/book-card/book-card.ts
@@ -11,11 +11,17 @@ export class BookCard {
   // Input: hier fließen Daten hinein von der Elternkomponente
   // von oben nach unten
   readonly book = input.required<Book>();
+  readonly min = input.required<number>();
+  readonly max = input.required<number>();
 
   // Output: hier fließen Daten hinaus zur Elternkomponente
   // von unten nach oben
   readonly rateUp = output<Book>();
   readonly rateDown = output<Book>();
+
+
+  readonly rateDownDisabled = computed(() => this.book().rating <= this.min());
+  readonly rateUpDisabled = computed(() => this.book().rating >= this.max());
 
   doRateUp() {
     this.rateUp.emit(this.book());
