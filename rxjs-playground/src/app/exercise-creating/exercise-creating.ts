1ed0b766e0ddef772f82948bbaa221f2cca737f1
Ferdinand Malcher <ferdinand@malcher.media>
2026-06-04 11:25:42 +0200

    RxJS: Intro

--- rxjs-playground/src/app/exercise-creating/exercise-creating.ts
+++ rxjs-playground/src/app/exercise-creating/exercise-creating.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of, from, timer, interval, ReplaySubject, map, filter } from 'rxjs';
+import { Observable, of, from, timer, interval, ReplaySubject, map, filter, Subscriber, Observer } from 'rxjs';
 
 import { HistoryWindow } from '../shared/history-window/history-window';
 
@@ -23,6 +23,52 @@ export class ExerciseCreating {
      */
 
     /******************************/
+
+    // of('Dresden', 'Leipzig', 'Nürnberg')
+    // interval(1000)      // ---0---1---2---3---4---5---6---7---8---9 ...
+    // timer(3000)         // ---------0|
+    // timer(3000, 1000)   // ---------0---1---2---3---4---5---6 ...
+    // timer(0, 1000)      // 0---1---2---3---4---5---6 ...
+    
+    
+    timer(0, 1000).pipe(
+      map(e => e * 3),
+      filter(e => e % 2 === 0)
+    ).subscribe({
+      next: e => this.log(e),
+      complete: () => this.log('COMPLETE')
+    })
+
+
+    /******************************/
+
+    function producer(sub: Subscriber<number>) {
+      const result = Math.random();
+      sub.next(result);
+      sub.next(5);
+      sub.next(10);
+      sub.next(20);
+
+      setTimeout(() => sub.next(100), 2000)
+      setTimeout(() => sub.complete(), 4000)
+    }
+
+
+    const obs: Observer<number> = {
+      next: (e: number) => console.log(e),
+      error: (err: any) => console.error(err),
+      complete: () => console.log('FERTIG')
+    };
+
+    // producer(obs);
+    // Finnische Notation
+    const myObs$ = new Observable(producer);
+    // myObs$.subscribe(obs);
+
+
+    new Observable<number>(sub => {
+      sub.next(30000);
+    })
 
     
     /******************************/
